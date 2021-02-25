da7740705c49d4bb0d67e9963531a5679271732b
0x4bd0 <abdoumjr@gmail.com>
2021-02-25 21:21:23 +0100

    setup reducer

--- src/contexts/list.js
+++ src/contexts/list.js
@@ -1,15 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 
 export const ListContext = createContext()
 
+const  defaultValue = [{id:1,text:"do this and that"}]
+const themeReducer = (state,action) => {
+   switch(action.type){
+       case 'insert' :
+           return 'x'
+        case 'reset' : 
+          return defaultValue
+   }
+}
+
 
 const ListContextProvider = (props) => {
 
-    let [list,setList] = useState([{id:1,text:"do this and that"}])
+    let [list,daspatch] = useReducer(themeReducer, defaultValue)
 
     return (  
-        <ListContext.Provider    value={{list}}>
+        <ListContext.Provider    value={{list,daspatch}}>
             {props.children}
         </ListContext.Provider>
     )
