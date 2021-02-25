29178a5d65a2eda727f7e82dd91a2507eef2fea4
0x4bd0 <abdoumjr@gmail.com>
2021-02-25 20:29:39 +0100

    setup core components

---
+++ src/components/wrapper.js
@@ -0,0 +1,13 @@
+import Header from "./header";
+import List from "./list";
+
+const Wrapper = () => {
+    return ( 
+        <div className="wrapper">
+        <Header></Header>
+        <List></List>
+        </div>
+     );
+}
+ 
+export default Wrapper;
